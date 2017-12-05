ac521f0c338a407913e69fb96e010c111414bc9b
iain26 <icotte200@caledonian.ac.uk>
2017-12-05 05:36:56 +0000

    NeedsWork
    unfinished - rhuairidh's game design (all nighters are tough)

--- Assets/Scripts/MainLevel/Manager.cs
+++ Assets/Scripts/MainLevel/Manager.cs
@@ -18,13 +18,15 @@ public class Manager : MonoBehaviour {
 
     public float speed = 1f;
 
-    public bool sampling = false;
+    bool sampling = true;
 
     GameObject samplingObject;
+    GameObject samplingButton;
 
     bool building = false;
 
     GameObject buildingMenuObject;
+    GameObject BuildingButton;
 
     bool analysing = false;
 
@@ -41,6 +43,7 @@ public class Manager : MonoBehaviour {
     Text gameTimeT;
     int gameTimeMin = 0;
     float gameTimeSec = 0;
+    float timeSec = 0;
 
     // Use this for initialization
     void Start () {
@@ -89,10 +92,16 @@ public class Manager : MonoBehaviour {
         censusMenuObject = GameObject.Find("CensusMenu");
         QuittingComfirmationObject = GameObject.Find("QuitComfirm");
 
-        satisfactionIm = GameObject.Find("HappinessMeter").GetComponent<Image>();
+        samplingButton = GameObject.Find("SamplingButton");
+        BuildingButton = GameObject.Find("BuildingMenuButton");
+
+        samplingObject.SetActive(sampling);
+        samplingButton.SetActive(false);
+
+        //satisfactionIm = GameObject.Find("HappinessMeter").GetComponent<Image>();
         moneyT = GameObject.Find("Money").GetComponent<Text>();
         populationT = GameObject.Find("Population").GetComponent<Text>();
-        foodT = GameObject.Find("Food").GetComponent<Text>();
+        //foodT = GameObject.Find("Food").GetComponent<Text>();
 
         gameTimeT = GameObject.Find("GameTimeT").GetComponent<Text>();
     }
@@ -113,6 +122,8 @@ public class Manager : MonoBehaviour {
 
     public void Building()
     {
+        BuildingButton.SetActive(false);
+        samplingButton.SetActive(true);
         if (!building)
         {
             analysing = false;
@@ -127,6 +138,8 @@ public class Manager : MonoBehaviour {
 
     public void Sampling()
     {
+        samplingButton.SetActive(false);
+        BuildingButton.SetActive(true);
         if (!sampling)
         {
             analysing = false;
@@ -156,12 +169,12 @@ public class Manager : MonoBehaviour {
         //satisfactionIm.fillAmount = fSatisfaction;
         float needle = fSatisfaction - 0.5f;
         //satisfactionIm.transform.SetPositionAndRotation(satisfactionIm.transform.position, new Quaternion(0,0, 90f*dial,0));
-        satisfactionIm.transform.localRotation = Quaternion.Euler(0, 0, -180f * needle);
+        //satisfactionIm.transform.localRotation = Quaternion.Euler(0, 0, -180f * needle);
         int iMoney = (int)fMoney;
         moneyT.text = iMoney.ToString();
         populationT.text = fPopulation.ToString();
         int iFood = (int)fFood;
-        foodT.text = iFood.ToString();
+        //foodT.text = iFood.ToString();
 
         string timeS;
         gameTimeSec += Time.deltaTime;
@@ -173,93 +186,106 @@ public class Manager : MonoBehaviour {
                 gameTimeMin++;
             }
         }
-        int gameTimeSecInt = (int)gameTimeSec;
-        timeS = gameTimeMin.ToString() + ":" + gameTimeSecInt.ToString();
-        if (gameTimeSec < 10)
-        {
-            timeS = gameTimeMin.ToString() + ":0" + gameTimeSecInt.ToString();
-        }
-        gameTimeT.text = "Total Game Time:  " + timeS;
+        float displayTime = 60f / timeSec;
+        GameObject.Find("TimeFillBar").GetComponent<Image>().fillAmount = 1f - displayTime;
+        //int gameTimeSecInt = (int)gameTimeSec;
+        //timeS = gameTimeMin.ToString() + ":" + gameTimeSecInt.ToString();
+        //if (gameTimeSec < 10)
+        //{
+        //    timeS = gameTimeMin.ToString() + ":0" + gameTimeSecInt.ToString();
+        //}
+        //gameTimeT.text = "Total Game Time:  " + timeS;
     }
 
     void CalculateSatisfaction()
     {
-        if (!starving)
-        {
-            fSatisfaction += 0.01f * Time.deltaTime;
-        }
-        else
-        {
-            fSatisfaction -= 0.01f * Time.deltaTime;
-        }
+        //if (!starving)
+        //{
+        //    fSatisfaction += 0.01f * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    fSatisfaction -= 0.01f * Time.deltaTime;
+        //}
 
-        if(fSatisfaction > 1f)
+        //if(fSatisfaction > 1f)
+        //{
+        //    fSatisfaction = 1f;
+        //}
+        //if (fSatisfaction <= 0f)
+        //{
+        //    //Screen.orientation = ScreenOrientation.Portrait;
+        //    Application.LoadLevel("Lose");
+        //}
+        //Debug.Log(fSatisfaction);
+
+    }
+
+    //IEnumerator TakeAwayFood()
+    //{
+    //    if (waited)
+    //    {
+    //        waited = false;
+    //        yield return new WaitForSeconds(10f);
+    //        if (fSatisfaction > 0f)
+    //        {
+    //            if (fSatisfaction > 0.5f)
+    //            {
+    //                if (fSatisfaction > 0.7f)
+    //                {
+    //                    fFood -= 3.5f * fPopulation;
+    //                }
+    //                else
+    //                {
+    //                    fFood -= 2.25f * fPopulation;
+    //                }
+    //            }
+    //            else
+    //            {
+    //                fFood -= 1.5f * fPopulation;
+    //            }
+    //            if(fFood < 0)
+    //            {
+    //                starving = true;
+    //            }
+    //            else
+    //            {
+    //                starving = false;
+    //            }
+    //            if (fFood <= 0)
+    //            {
+    //                fFood = 0f;
+    //            }
+    //        }
+    //        waited = true;
+    //    }
+    //}
+
+    void Timer()
+    {
+        string sTime;
+        timeSec += Time.deltaTime;
+        if (timeSec >= 60)
         {
-            fSatisfaction = 1f;
-        }
-        if (fSatisfaction <= 0f)
-        {
-            //Screen.orientation = ScreenOrientation.Portrait;
             Application.LoadLevel("Lose");
         }
-        //Debug.Log(fSatisfaction);
     }
-
-    IEnumerator TakeAwayFood()
-    {
-        if (waited)
-        {
-            waited = false;
-            yield return new WaitForSeconds(10f);
-            if (fSatisfaction > 0f)
-            {
-                if (fSatisfaction > 0.5f)
-                {
-                    if (fSatisfaction > 0.7f)
-                    {
-                        fFood -= 3.5f * fPopulation;
-                    }
-                    else
-                    {
-                        fFood -= 2.25f * fPopulation;
-                    }
-                }
-                else
-                {
-                    fFood -= 1.5f * fPopulation;
-                }
-                if(fFood < 0)
-                {
-                    starving = true;
-                }
-                else
-                {
-                    starving = false;
-                }
-                if (fFood <= 0)
-                {
-                    fFood = 0f;
-                }
-            }
-            waited = true;
-        }
-    }
-
 
     private void FixedUpdate()
     {
-        fFood += (speed */* Random.Range(0.5f, 0.7f)*/ 0.3f) * Time.deltaTime * fNumBuilding;
+        //fFood += (speed */* Random.Range(0.5f, 0.7f)*/ 0.3f) * Time.deltaTime * fNumBuilding;
     }
 
     // Update is called once per frame
     void Update ()
     {
-        StartCoroutine(TakeAwayFood());
+        //StartCoroutine(TakeAwayFood());
         //fFood -= (speed * Random.Range(0.5f, 0.7f)) * Time.deltaTime * fPopulation;
 
         //fMoney += (speed * 0.75f) * Time.deltaTime * fPopulation;
         //fMoney -= (speed * 0.45f) * Time.deltaTime * fNumBuilding;
         CalculateSatisfaction();
+        Timer();
         SetMeters();
 
         if (samplingObject != null)
